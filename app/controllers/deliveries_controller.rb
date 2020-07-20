1d2f9a29d30922ba5358eefec5b990197e95084a
保田壮太 <yasudasota@MacBook-Pro.local>
2020-07-20 16:32:17 +0900

    add render

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,2 +1,16 @@
 class DeliveriesController < ApplicationController
+	before_action :authenticate_customer!
+
+	def index
+		@deliveries = Address.where(customer_id: current_customer.id)
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	private
+
 end

e3a595fd448c7e546ab2657f345f93e4feeb389d
vagrant <vagrant@localhost.localdomain>
2020-07-21 06:55:58 +0000

    add cart_items

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,36 @@
 class CartItemsController < ApplicationController
+	def index
+		@cart_items = CartItem.all
+	end
+
+	def create
+		@cart_item = CartItem.new(cart_item_params)
+		@cart_item.customer_id = current_customer.id
+		@cart_item.save
+		redirect_to cart_items_path(current_customer)
+	end
+
+	def update
+		@cart_item = CartItem.find(cart_item_params)
+		@cart_item.update
+		redirect_to redirect_to cart_items_path(current_customer)
+	end
+
+	def destroy
+		@cart_item = CartItem.find(params[:id])
+		@cart_item.destroy
+		redirect_to cart_items_path(current_customer)
+	end
+
+	def destroy_all
+		@cart_items = current_customer.cart_items
+		@cart_items.destroy_all
+		redirect_to cart_items_path(current_customer)
+	end
+
+	private
+
+	def cart_item_params
+	params.require(:cart_item).permit(:customer_id, :sweet_id, :sweet_count)
+	end
 end

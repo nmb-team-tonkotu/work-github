b46d765483eec2186b2b1b0f6cb2eb6af0b2b1c0
masudayouko <youyo1204@gmail.com>
2020-07-21 06:30:33 +0000

    add customers_sweet

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,2 +1,19 @@
 class SweetsController < ApplicationController
+	def index
+		@sweets = Sweet.all
+
+	end
+
+	def show
+		@sweet = Sweet.find(params[:id])
+	end
+
+	def search
+		if params[:genre].present?
+			@sweets = Sweet.where('genre LIKE ?',"%#{params[:genre]}%")
+		else
+			@sweets = Sweet.none
+		end
+	end
+
 end

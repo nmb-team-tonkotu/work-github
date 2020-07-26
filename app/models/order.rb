53a8e4ff8bb0cab2e27e9610d920500338eb1a48
vagrant <vagrant@localhost.localdomain>
2020-07-26 04:40:59 +0000

    finish admin_order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
 	has_many :order_sweets
 	has_many :sweets
 
-	enum status: [:入金待ち, :入金確認, :製作中, :発送準備中, :発送済み]
+	enum status: {"入金待ち": 0,"入金確認": 1 ,"製作中": 2,"発送準備中": 3, "発送済み": 4}
 	enum payment_method: [:クレジット, :銀行支払]
 
 

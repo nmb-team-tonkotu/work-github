4a7656a9c1ac1c86646d82e925bfa9ec5027be6c
masudayouko <youyo1204@gmail.com>
2020-07-26 05:01:06 +0000

    repair2

--- app/models/order_sweet.rb
+++ app/models/order_sweet.rb
@@ -2,4 +2,10 @@ class OrderSweet < ApplicationRecord
 	belongs_to :sweet
 	belongs_to :order
 
+		enum create_status: [:着手不可, :製作待ち, :製作中, :製作完了]
+
+	def order_sum
+		self.price * self.count
+	end
+
 end

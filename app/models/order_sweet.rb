369273d469f83d6950d7a103c4f7c6b77ed77cdd
masudayouko <youyo1204@gmail.com>
2020-07-26 05:07:32 +0000

    repair3

--- app/models/order_sweet.rb
+++ app/models/order_sweet.rb
@@ -2,10 +2,9 @@ class OrderSweet < ApplicationRecord
 	belongs_to :sweet
 	belongs_to :order
 
-		enum create_status: [:着手不可, :製作待ち, :製作中, :製作完了]
+	enum create_status: [:着手不可, :製作待ち, :製作中, :製作完了]
 
 	def order_sum
-		self.price * self.count
+			self.price * self.count
 	end
-
 end

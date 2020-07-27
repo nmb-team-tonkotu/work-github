45ea5f43df27585f99f28745b98f084536565a8b
vagrant <vagrant@localhost.localdomain>
2020-07-27 05:48:26 +0000

    add bootstrap

--- app/controllers/admins/order_sweets_controller.rb
+++ app/controllers/admins/order_sweets_controller.rb
@@ -2,6 +2,7 @@ class Admins::OrderSweetsController < ApplicationController
     def update
         order_sweet = OrderSweet.find(params[:id])
         order_sweet.update(order_sweet_params)
+        flash[:notice] = "Created successfully !!"
         redirect_to admins_order_path(order_sweet.order_id)
     end
     private
